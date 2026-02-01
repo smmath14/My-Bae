d1528f37042cc2b81404146e9acb267f6ca6b82c
Sourabh Mathur <73380583+smmath14@users.noreply.github.com>
2026-02-01 11:04:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const noBtn = document.getElementById("no");
+const yesBtn = document.getElementById("yes");
+const msg = document.getElementById("message");
+
+noBtn.addEventListener("mouseover", () => {
+    const x = Math.random() * 200 - 100;
+    const y = Math.random() * 200 - 100;
+    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+yesBtn.addEventListener("click", () => {
+    msg.innerHTML = "Thank you for forgiving me ❤️ I promise to do better.";
+});
